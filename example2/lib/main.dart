be219f1f0e376bb13a6eb2f358e590254e5294da
eduardoagas <eagasilva12@gmail.com>
2023-07-31 06:06:14 -0300

    first commit

---
+++ example2/lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+void main() {
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+extension OptionalInfixAddition<T extends num> on T? {
+  T? operator +(T? other) {
+    final shadow = this;
+    if (shadow != null) {
+      return shadow + (other ?? 0) as T;
+    } else {
+      return null;
+    }
+  }
+}
+
+class Counter extends StateNotifier<int?> {
+  Counter() : super(null);
+  void increment() => state = state == null ? 1 : state + 1;
+}
+
+final counterProvider =
+    StateNotifierProvider<Counter, int?>((ref) => Counter());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.dark,
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends ConsumerWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Consumer(builder: (context, ref, child) {
+          final count = ref.watch(counterProvider);
+          final text = count == null ? 'Press the button' : count.toString();
+          return Text(text);
+        }),
+      ),
+      body: Column(
+        children: [
+          TextButton(
+              onPressed: () {
+                ref.read(counterProvider.notifier).increment();
+              },
+              child: const Text('Increment Counter'))
+        ],
+      ),
+    );
+  }
+}
