4a92be20ac601db1df5ea7f9e02c4eec5e6eca68
eduardoagas <eagasilva12@gmail.com>
2023-08-03 02:04:17 -0300

    auth (eternal) configurations

--- instagram_clone/lib/main.dart
+++ instagram_clone/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -7,7 +8,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
