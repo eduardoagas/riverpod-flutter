dceabb4b668c07143249e9c4fb34ba117e030aab
eduardoagas <eagasilva12@gmail.com>
2023-08-08 16:40:28 -0300

    thumbnails and stuff

--- instagram_clone/lib/views/components/constants/strings.dart
+++ instagram_clone/lib/views/components/constants/strings.dart
@@ -5,11 +5,12 @@ class Strings {
   static const allowLikesTitle = 'Allow likes';
   static const allowLikesDescription =
       'By allowing likes, users will be able to press the like button on your post.';
-  static const allowLikesStorageKey = 'allow_likes';
+  static const allowLikesStorageKey = 'allow_likes'; //not to be translated
   static const allowCommentsTitle = 'Allow comments';
   static const allowCommentsDescription =
       'By allowing comments, users will be able to comment on your post.';
-  static const allowCommentsStorageKey = 'allow_comments';
+  static const allowCommentsStorageKey =
+      'allow_comments'; //not to be translated
 
   static const comment = 'comment';
 
---
+++ instagram_clone/lib/views/components/post/posts_grid_view.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/views/components/post/post.thumbnail.dart';
+
+import '../../../state/posts/post.dart';
+
+class PostsGridView extends StatelessWidget {
+  final Iterable<Post> posts;
+  const PostsGridView({super.key, required this.posts});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      padding: const EdgeInsets.all(8),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3, mainAxisSpacing: 8.0, crossAxisSpacing: 8.0),
+      itemCount: posts.length,
+      itemBuilder: (context, index) {
+        final post = posts.elementAt(index);
+        return PostThumbnailView(
+            post: post,
+            onTapped: () {
+              //TODO: navigate to the post details view
+            });
+      },
+    );
+  }
+}
