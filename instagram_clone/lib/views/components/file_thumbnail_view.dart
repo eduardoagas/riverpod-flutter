beac0ca12a1eabf577824a0eec52a47e1122448a
eduardoagas <eagasilva12@gmail.com>
2023-08-11 08:22:20 -0300

    hmgttibah

---
+++ instagram_clone/lib/views/components/file_thumbnail_view.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:instagram_clone/state/image_upload/models/image_with_aspect_ratio.dart';
+import 'package:instagram_clone/state/image_upload/models/thumbnail_request.dart';
+import 'package:instagram_clone/state/image_upload/providers/thumbnail_provider.dart';
+import 'package:instagram_clone/views/components/animations/error_animation_view.dart';
+import 'package:instagram_clone/views/components/animations/loading_animation_view.dart';
+import 'package:instagram_clone/views/components/animations/small_error_animation_view.dart';
+
+class FileThumbnailView extends ConsumerWidget {
+  final ThumbnailRequest thumbnailRequest;
+  const FileThumbnailView({super.key, required this.thumbnailRequest});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final thumbnail = ref.watch(thumbnailProvider(
+      thumbnailRequest,
+    ));
+    return thumbnail.when(
+      data: (imageWithAspectRatio) {
+        return AspectRatio(
+          aspectRatio: imageWithAspectRatio.aspectRatio,
+          child: imageWithAspectRatio.image,
+        );
+      },
+      error: (error, stackTrace) => const ErrorAnimationView(),
+      loading: () => const LoadingAnimationView(),
+    );
+  }
+}
